e00942c1e34ea988ed51c4b8c6c5d8dde673bec7
shawnbmccarthy <sbmccarth@gmail.com>
2023-10-15 23:13:21 -0400

    added support for namespaces, added docs

--- src/components/viam_ros_node.py
+++ src/components/viam_ros_node.py
@@ -1,4 +1,13 @@
+"""
+the ViamRosNode will represent the node that will publish and subscribe to
+ROS nodes as well as manage other aspects of ROS in the future.
+
+Ideally we will have a single ROS node but this will change based on the need
+of the architectures we come across
+"""
 from rclpy.node import Node
+import os
+from typing_extensions import Self
 from utils import RclpyNodeManager
 
 
@@ -6,18 +15,42 @@ class ViamRosNode(Node):
     """
     ViamRosNode
     A single node to integrate with the ROS robot
-
-    TODO: Test namespace changes
     """
     node = None
 
     @classmethod
-    def get_viam_ros_node(cls, namespace: str = '', node_name: str = 'viam_ros_node', enable_rosout: bool = True):
+    def get_viam_ros_node(
+        cls,
+        namespace: str = '',
+        node_name: str = '',
+        enable_rosout: bool = True
+    ) -> Self:
+        """
+        get_viam_ros_node attempts to get an existing ros node and if one
+        does not exist it will create it
+        """
         if cls.node is None:
+            # this check is also done in main.py - leave here for testing purposes
+            if node_name is None or node_name == '':
+                if 'VIAM_NODE_NAME' in os.environ and os.environ['VIAM_NODE_NAME'] != '':
+                    nodename = os.environ['VIAM_NODE_NAME']
+                else:
+                    raise Exception(
+                        'cannot find valid viam node name in environment,'
+                        ' please set VIAM_NODE_NAME\nsee install instructions'
+                    )
             cls.node = ViamRosNode(namespace, node_name, enable_rosout)
         return cls.node
 
-    def __init__(self, namespace: str = '', node_name: str = 'viam_ros_node', enable_rosout: bool = True):
+    def __init__(
+        self,
+        namespace: str = '',
+        node_name: str = '',
+        enable_rosout: bool = True
+    ) -> None:
+        """
+        __init__ creates the node
+        """
         super().__init__(node_name, namespace=namespace, enable_rosout=enable_rosout)
         rclpy_mgr = RclpyNodeManager.get_instance()
         rclpy_mgr.spin_and_add_node(self)
