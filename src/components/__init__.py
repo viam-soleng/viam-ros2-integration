4779ec5cfb7f4512a806d771805276b408237080
Matt Dannenberg <matt@viam.com>
2023-11-17 11:02:59 -0500

    add simple RosTopicPublisher component (#11)
    * takes ros_topic and publish_rate as component arguments
    * do_command formatted {"send": "<your message here>"} will send your message to ros_topic at publish_rate

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -2,5 +2,6 @@ from .ros_base import RosBase
 from .ros_imu import RosImu
 from .ros_lidar import RosLidar
 from .ros_sensor import RosSensor
+from .ros_topic_publisher import RosTopicPublisher
 from .viam_ros_node import ViamRosNode
 from .ros_camera import RosCamera
